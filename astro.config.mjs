831be3cf5f650dbcfada186d75c1cb0087a499f1
Nicolas Pedranti <nicopedranti@gmail.com>
2024-11-05 12:55:55 -0300

    feat: add Astro island with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://delightful-gecko-4478a2.netlify.app/blog/"
+  site: "https://delightful-gecko-4478a2.netlify.app/blog/",
+  integrations: [preact()]
 });
